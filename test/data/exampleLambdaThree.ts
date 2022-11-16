f0bfb53bb8379ea8fb7de343b11eac3a764a10c7
White Autumn <white@autumn.dev>
2022-11-16 08:57:42 +0100

    Wipe all files

--- test/data/exampleLambdaThree.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export async function handler() {
-  console.log("Hello!");
-
-  const result = await axios.get("https://swapi.dev/api/planets/1/");
-  console.log(result);
-};
--- test/data/exampleSharedCode.js
+++
@@ -1,5 +0,0 @@
-const { default: Exception } = require("exceptions-with-cause");
-
-module.exports.throwAnException = () => {
-  throw new Exception("Example!");
-};
